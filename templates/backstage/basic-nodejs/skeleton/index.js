219e5f4d3514e06de29f998908479fba1e799418
Anurak C. <anurak@dsgpay.com>
2025-08-20 15:59:29 +0700

    update

--- templates/backstage/basic-nodejs/skeleton/index.js
+++ templates/backstage/basic-nodejs/skeleton/index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from {{ values.name }} running on Cnoe.io!');
+  res.send('Hello from ${{ values.name }} running on Cnoe.io!');
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`App {{ values.name }} listening on port ${port}`);
+  console.log(`App ${{ values.name }} listening on port ${port}`);
 });
